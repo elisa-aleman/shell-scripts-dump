b0a2d6b19d488d272b46929059427fc1dbb37999
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2021-10-03 18:02:52 +0900

    Added files

---
+++ set-terminal-back-lab.sh
@@ -0,0 +1,9 @@
+mv ~/.bash_profile ~/.bash_profile_home
+mv ~/.zprofile ~/.zprofile_home
+mv ~/.curlrc ~/.curlrc_home
+mv ~/.gitconfig ~/.gitconfig_home
+
+mv ~/.bash_profile_lab ~/.bash_profile
+mv ~/.zprofile_lab ~/.zprofile
+mv ~/.curlrc_lab ~/.curlrc 
+mv ~/.gitconfig_lab ~/.gitconfig
---
+++ set_terminal_home.sh
@@ -0,0 +1,9 @@
+mv ~/.bash_profile ~/.bash_profile_lab
+mv ~/.zprofile ~/.zprofile_lab
+mv ~/.curlrc ~/.curlrc_lab
+mv ~/.gitconfig ~/.gitconfig_lab
+
+mv ~/.bash_profile_home ~/.bash_profile
+mv ~/.zprofile_home ~/.zprofile
+mv ~/.curlrc_home ~/.curlrc
+mv ~/.gitconfig_home ~/.gitconfig
