4b7f21482b2c02f178a96ff416e4475ff4be58be
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2022-09-15 11:21:24 +0900

    quote mark

--- git-update-setup-manuals.sh
+++ git-update-setup-manuals.sh
@@ -3,28 +3,28 @@
 while getopts ":m:" opt; do
     case $opt in
         m)
-            cd ~/Github-personal/mac-proxy-lab-setup
+            cd ~/Github-personal/tutorials/mac-proxy-lab-setup
             echo "cd ~/Github-personal/tutorials/mac-proxy-lab-setup/" >&2
             git add README.md
             echo "git add README.md" >&2
             git commit -m "$OPTARG"
-            echo "git commit -m "$OPTARG"" >&2
+            echo 'git commit -m "'$OPTARG'"' >&2
             echo "git push" >&2
             git push
-            cd ~/Github-personal/win-data-science-py-setup
+            cd ~/Github-personal/tutorials/win-data-science-py-setup
             echo "cd ~/Github-personal/tutorials/win-data-science-py-setup" >&2
             git add README.md
             echo "git add README.md" >&2
             git commit -m "$OPTARG"
-            echo "git commit -m "$OPTARG"" >&2
+            echo 'git commit -m "'$OPTARG'"' >&2
             echo "git push" >&2
             git push
-            cd ~/GitLab-work/win-data-science-py-setup
+            cd ~/GitLab-work/tutorials/win-data-science-py-setup
             echo "cd ~/GitLab-work/tutorials/win-data-science-py-setup" >&2
             git add README.md
             echo "git add README.md" >&2
             git commit -m "$OPTARG"
-            echo "git commit -m "$OPTARG"" >&2
+            echo 'git commit -m "'$OPTARG'"' >&2
             echo "git push" >&2
             git push
             cd ~ 
