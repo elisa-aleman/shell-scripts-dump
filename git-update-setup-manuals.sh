4eea933d2e8e617cdcd889c5b1ec7adfec407442
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2022-09-15 10:34:14 +0900

    commit several gits at the same time with optional message

---
+++ git-update-setup-manuals.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+while getopts ":m:" opt; do
+    case $opt in
+        m)
+            cd ~/Github-personal/mac-proxy-lab-setup
+            echo "cd ~/Github-personal/tutorials/mac-proxy-lab-setup/" >&2
+            git add README.md
+            echo "git add README.md" >&2
+            git commit -m "$OPTARG"
+            echo "git commit -m "$OPTARG"" >&2
+            echo "git push" >&2
+            git push
+            cd ~/Github-personal/win-data-science-py-setup
+            echo "cd ~/Github-personal/tutorials/win-data-science-py-setup" >&2
+            git add README.md
+            echo "git add README.md" >&2
+            git commit -m "$OPTARG"
+            echo "git commit -m "$OPTARG"" >&2
+            echo "git push" >&2
+            git push
+            cd ~/GitLab-work/win-data-science-py-setup
+            echo "cd ~/GitLab-work/tutorials/win-data-science-py-setup" >&2
+            git add README.md
+            echo "git add README.md" >&2
+            git commit -m "$OPTARG"
+            echo "git commit -m "$OPTARG"" >&2
+            echo "git push" >&2
+            git push
+            cd ~ 
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+
